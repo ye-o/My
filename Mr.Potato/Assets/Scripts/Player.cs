7ce3484b502e207fe8010c80324f74245a12e097
ye-o <82939489+ye-o@users.noreply.github.com>
2021-04-25 20:51:03 +0800

    背景、前景、主角

---
+++ Mr.Potato/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D heroBody;
+    public float force = 50;
+    void Start()
+    {
+        heroBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        heroBody.AddForce(Vector2.right * h * force);
+    }
+}
