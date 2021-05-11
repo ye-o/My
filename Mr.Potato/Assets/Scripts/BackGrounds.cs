b9438eb4f45d2a73d068ab6637a00aca28cff3b6
ye-o <82939489+ye-o@users.noreply.github.com>
2021-05-11 19:38:49 +0800

    摄像机跟随和背景运动补偿

---
+++ Mr.Potato/Assets/Scripts/BackGrounds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGrounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] backGrounds;
+    public float fparallax = 0.4f;
+    public float layerFraction = 5f;
+    public float fSmooth = 5f;
+
+    Transform cam;
+    Vector3 previousCamPos;
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    private void Start()
+    {
+        previousCamPos = cam.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fParallax = (previousCamPos.x - cam.position.x) * fparallax;
+        for(int i=0;i< backGrounds.Length; i++)
+        {
+            float fNewX = backGrounds[i].position.x + fParallax * (1 + i * layerFraction);
+            Vector3 newPos = new Vector3(fNewX, backGrounds[i].position.y, backGrounds[i].position.z);
+            backGrounds[i].position = Vector3.Lerp(backGrounds[i].position, newPos, fSmooth * Time.deltaTime);
+        }
+        previousCamPos = cam.position;
+    }
+}
---
+++ Mr.Potato/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTran;  //主角的transform
+    public float maxDistanceX = 2;
+    public float maxDistanceY = 2;
+    public float xSpeed = 4;
+    public float ySpeed = 4;
+    public Vector2 maxXandY;
+    public Vector2 minXandY = new Vector2(-8, 8);
+    // Start is called before the first frame update
+    private bool NeedMoveX()
+    {
+        bool bMove = false;
+        if (Mathf.Abs(transform.position.x - playerTran.position.x) > maxDistanceX)
+            bMove = true;
+        else
+            bMove = false;
+        return bMove;
+    }
+    private bool NeedMoveY()
+    {
+        bool gMove = false;
+        if (Mathf.Abs(transform.position.y - playerTran.position.y) > maxDistanceY)
+            gMove = true;
+        else
+            gMove = false;
+        return gMove;
+    }
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        //playerTran = GameObject.Find("Hero").transform;
+        playerTran = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void TrackPlayer()
+    {
+        float newX = transform.position.x;
+        float newY = transform.position.y;
+        if (NeedMoveX())
+            newX = Mathf.Lerp(transform.position.x, playerTran.position.x, xSpeed * Time.deltaTime);
+        newX = Mathf.Clamp(newX, minXandY.x, maxXandY.x);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        if (NeedMoveY())
+            newY = Mathf.Lerp(transform.position.y, playerTran.position.y, xSpeed * Time.deltaTime);
+        newY = Mathf.Clamp(newY, minXandY.y, maxXandY.y);
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+    }
+    private void FixedUpdate()
+    {
+        TrackPlayer();
+
+
+    }
+}
